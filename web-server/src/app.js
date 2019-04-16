c552a00f16b2f5221bcd4a85ad2afe07dd9e29cb
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2019-04-16 12:29:02 +0700

    finish advance handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs');
-//app.use(express.static(publicDirectoryPath)); look for html file in specific folder (we use hbs instead)
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath)); //look for html file in specific folder (we use hbs instead)
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -23,7 +33,9 @@ app.get('/about',(req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some help'
+        helpText: 'This is some help',
+        title: 'Help', 
+        name: 'Sethanant'
     })
 })
 

bd6aca94222ef08eadc17ff9bc1718ba293cd307
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-04-19 21:19:45 +0700

    complete all CRUD function

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,18 +21,6 @@ app.post('/users', async (req, res) => {
     }
 })
 
-//For create new task
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-    try{
-        await task.save()
-        res.status(201).send(task)
-    }
-    catch(err) {
-        res.status(400).send(err)
-    }
-})
-
 //Fetch all users
 app.get('/users', async (req, res) => {
     try{
@@ -46,11 +34,11 @@ app.get('/users', async (req, res) => {
 
 //Fetch user by id
 app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+    const _id = req.params.id //from the /users/:id
     try{
         const user = await User.findById(_id)
-        if(!user) return res.status(404).send()
-
+        if(!user) return res.status(404).send() //if user = null is mean that there is not this user in db so we just send 404 not found
+        //this line below will not run when 'return' is excute (when user = null and trigger if statement)
         res.send(user)
     }
     catch(err){
@@ -58,6 +46,56 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+//Update a user
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body) //return a string of keys of that object
+    const allowUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every(update => allowUpdates.includes(update))
+    //check if the updates is valid to do
+    if(isValidOperation == false){
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    //actually update the data
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+//Delete the user
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+
+})
+
+
+
+/////////////TASKS SECTION///////////////////
+
+
+
+//For create new task
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }
+    catch(err) {
+        res.status(400).send(err)
+    }
+})
 
 //Fetch all tasks
 app.get('/tasks', async (req, res) => {
@@ -83,7 +121,39 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+//Update a task
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const allowUpdates = ['description', 'completed'];
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every(update => allowUpdates.includes(update))
 
+    if(isValidOperation == false){
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        if(!task) return res.status(404).send()
+        res.send(task)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+
+})
+
+//Delete the task 
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task) return res.status(404).send()
+        res.send(task)
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+})
 
 
 

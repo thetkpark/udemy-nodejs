a298e20ab74c0e70e07f1a5a278e83fb8ab514a5
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-05-01 10:32:59 +0700

    add droping db in beforeEach + expect 400 from invalid login

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,18 @@
-const request = require('supertest');
+const request = require('supertest'); //used for sent request to express
 const app = require('../src/app');
+const User = require('../src/models/users');
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: 'red12345'
+}
+
+//Delete all the data before running every test
+beforeEach(async () => {
+    await User.deleteMany() //Drop all data
+    await new User(userOne).save() //create user for another test
+})
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -8,6 +21,20 @@ test('Should sign up a new user', async () => {
         password: 'asdfghjkl;'
     }).expect(201)
     
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'passwordThatGonnaFailed'
+    }).expect(400)
 })
 
 
